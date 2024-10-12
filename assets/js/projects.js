c009a68d24389c9b6acb081b3aaa7cd78fd537b5
Ezra Luvaha <luvaha456@gmail.com>
2024-10-12 14:35:36 +0300

    Removed unnecessary projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -9,44 +9,12 @@ const projects = [
         link: 'https://olympics-website.vercel.app/'
     },
     {
-        image: 'assets/img/Digital-Preview.png',
-        alt: 'Digital Agency Website',
-        subtitle: 'Website',
-        title: 'Digital Agency Website',
-        description: 'Modern landing page usig GSAP animations and Lenis Library for smoth scrolling.',
-        link: 'https://agency-landing-page-seven.vercel.app/'
-    },
-    {
-        image: 'assets/img/Marketing-Preview.png',
-        alt: 'Marketing Agency Website',
-        subtitle: 'Website',
-        title: 'Advantage Agency Website',
-        description: 'A professional modern marketing website portfolio that uses GSAP animation',
-        link: 'https://marketing-portfolio-five.vercel.app/'
-    },
-    {
         image: 'assets/img/Expense-Preview.png',
         alt: 'Expense Manager App',
         subtitle: 'Web App',
         title: 'Expense Manager App',
         description: 'This is a fullstack Expense Tracker App.',
         link: 'https://expense-tracker-app-topaz.vercel.app/'
-    },
-    {
-        image: 'assets/img/Timeline-Preview.png',
-        alt: 'Advanced Timeline Design',
-        subtitle: 'Landing Page',
-        title: 'Advanced Timeline Design',
-        description: 'This is a creative scroll video animation landing page.',
-        link: 'https://advanced-timeline-design.vercel.app/'
-    },
-    {
-        image: 'assets/img/Restaurant-Preview.png',
-        alt: 'Restaurant Website',
-        subtitle: 'Website',
-        title: 'Restaurant Website',
-        description: 'This is a restaurant website.',
-        link: 'https://restaurant-website-indol-kappa.vercel.app/'
     },
 
     // Add more projects here
